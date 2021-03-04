967c73a1d1c34ef574244c31be264f7aea3b3b96
Detar Borsa <detar.borsa@gmail.com>
2021-03-04 17:55:01 +0100

    docker run corrections

--- entrypoint.bash
+++ entrypoint.bash
@@ -3,4 +3,6 @@
 source /opt/ros/noetic/setup.bash
 source /root/ros_ws/devel/setup.bash
 
+export ROS_MASTER_URI=http://localhost:11311
+
 exec "$@"
--- run.bash
+++ run.bash
@@ -1,5 +1,7 @@
 docker run \
     -it \
+    --rm \
     --privileged \
     -v /dev:/dev \
+    --network host \
     platypous:latest roslaunch platy_bringup basic.launch
