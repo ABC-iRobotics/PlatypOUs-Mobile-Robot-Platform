cd9d344d383683ec96e7d42f3e36faaa95835169
borsasd <detar.borsa@gmail.com>
2021-04-18 19:46:47 +0200

    reorganized startup and utility scripts

---
+++ scripts/start_basic.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /root/ros_ws/devel/setup.bash
+
+roslaunch platypous_launch basic.launch
--- start_script.bash
+++
@@ -1,2 +0,0 @@
-roslaunch platypous_launch basic.launch &
-sleep 5 && cd /root/web && node server.js
