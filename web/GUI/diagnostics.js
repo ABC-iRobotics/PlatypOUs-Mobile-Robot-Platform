fa247c89572560ff72d5076df77a09c00e24ae66
Panna Zsoldos <zsoldos.panna@gmail.com>
2021-04-25 20:01:45 +0200

    added joystick teleop to web UI

--- web/GUI/diagnostics.js
+++ web/GUI/diagnostics.js
@@ -1,69 +1,22 @@
 Vue.component("diagnostics", {
   template: `
   <div>
-      
+    <b-container fluid>
+      <b-card>
+        <b-card-text>
+          
+        </b-card-text>
+      </b-card>
+    </b-container>
   </div>
   `,
   
   data(){
-    return {};
+    return {
+      
+    };
   },
   
   methods: {}
 });
 
-//~ <b-button v-b-toggle.coll-1 variant="primary">Go somewhere</b-button>
-      //~ <b-card-group deck>
-      
-        //~ <b-collapse id="coll-1">
-          //~ <b-card
-            //~ title="Diagnostics"
-            //~ img-alt="Image"
-            //~ img-top
-            //~ tag="article"
-            //~ style="max-width: 20rem;"
-            //~ class="mb-2"
-          //~ >
-            
-              //~ <b-card-text>
-                //~ View here some diagnostic data about PlatypOUs.
-              //~ </b-card-text>
-          //~ </b-card>
-          
-          //~ <b-card
-          //~ title="Card Title"
-          //~ img-src="https://picsum.photos/600/300/?image=25"
-          //~ img-alt="Image"
-          //~ img-top
-          //~ tag="article"
-          //~ style="max-width: 20rem;"
-          //~ class="mb-2"
-        //~ >
-          //~ <b-card-text>
-            //~ Some quick example text to build on the card title and make up the bulk of the card's content.
-          //~ </b-card-text>
-
-          //~ <b-button href="#" variant="primary">Go somewhere</b-button>
-        //~ </b-card>
-        
-        //~ <b-card
-          //~ title="Card Title"
-          //~ img-src="https://picsum.photos/600/300/?image=25"
-          //~ img-alt="Image"
-          //~ img-top
-          //~ tag="article"
-          //~ style="max-width: 20rem;"
-          //~ class="mb-2"
-        //~ >
-          //~ <b-card-text>
-            //~ Some quick example text to build on the card title and make up the bulk of the card's content.
-          //~ </b-card-text>
-
-          //~ <b-button href="#" variant="primary">Go somewhere</b-button>
-        //~ </b-card>
-          
-          
-        //~ </b-collapse>
-
-        
-      //~ </b-card-group deck>
