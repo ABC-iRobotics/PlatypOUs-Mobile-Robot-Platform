85f9cd325b289b674629cc02eb61641fd7079c95
Panna Zsoldos <zsoldos.panna@gmail.com>
2021-07-05 13:47:35 +0200

    show delay

--- web/GUI/main_vue.js
+++ web/GUI/main_vue.js
@@ -16,7 +16,7 @@ var App = new Vue({
   
   methods:{
     resetPingTimeout: function(){
-      this.ping_delay = round(this.ping_timeout, 2);
+      this.ping_delay = this.ping_timeout.toFixed(2);
       this.ping_timeout = 0;
     },
     
